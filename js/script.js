ff570e81e6de1adfa3ac98dd6a7dc970fa38a63c
v-eeru <133241025+v-eeru@users.noreply.github.com>
2025-05-14 15:26:22 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -31,6 +31,29 @@ select.addEventListener("change",()=>{
     }
 })
 
+const draggables = document.querySelectorAll('.draggable');
+draggables?.forEach(draggable => {
+    draggable.addEventListener('mousedown', function(e) {
+        const shiftX = e.clientX - draggable.getBoundingClientRect().left;
+        const shiftY = e.clientY - draggable.getBoundingClientRect().top;
+        function moveAt(pageX, pageY) {
+            draggable.style.left = pageX - shiftX + 'px';
+            draggable.style.top = pageY - shiftY + 'px';
+        }
+        function onMouseMove(e) {
+            moveAt(e.pageX, e.pageY);
+        }
+        document.addEventListener('mousemove', onMouseMove);
+        draggable.onmouseup = function() {
+            document.removeEventListener('mousemove', onMouseMove);
+            draggable.onmouseup = null;
+        };
+    });
+    draggable.ondragstart = function() {
+        return false;
+    };
+});
+
 
 
 
