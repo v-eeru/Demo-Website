7bb2b64584b8988f9699aa6c79c779d127d312d6
v-eeru <133241025+v-eeru@users.noreply.github.com>
2025-06-08 17:01:45 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
-// Aptrinsic SDK loader
 (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-NLM0CA3YQKIW-2");
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-8BLCMRAZRTFW-2");
 
 document.addEventListener("DOMContentLoaded", () => {
 
