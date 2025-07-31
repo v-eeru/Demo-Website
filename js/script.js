32bf223ef097b8c77072d395c6a3e95421193099
v-eeru <133241025+v-eeru@users.noreply.github.com>
2025-07-31 23:24:30 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 
 (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-  (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-CKALAZKJJKPI-2");
+      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
+    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
+    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-NLM0CA3YQKIW-2");
 
 
 document.addEventListener("DOMContentLoaded", function () {
