028df59eb24bf848b665ba77df46b7c2ca8d1da9
v-eeru <133241025+v-eeru@users.noreply.github.com>
2025-09-19 13:39:52 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-NLM0CA3YQKIW-2-3");
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-NLM0CA3YQKIW-2");
 
 
 document.addEventListener("DOMContentLoaded", function () {
