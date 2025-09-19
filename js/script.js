a20cdece071ff1770629017762b8f941fc5d633f
v-eeru <133241025+v-eeru@users.noreply.github.com>
2025-09-19 13:03:16 +0530

    Update script.js
    git please apply these changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 
-
-
   (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
