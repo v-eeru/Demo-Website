80118a79768dd42f28e2182f97807a5c21475073
v-eeru <133241025+v-eeru@users.noreply.github.com>
2025-10-06 21:34:28 +0530

    its completly new dear

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-(function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+
+  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-NLM0CA3YQKIW-2");
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-4YCPERINHU7C-2");
 
 document.addEventListener("DOMContentLoaded", function () {
   const fullUrl = window.location.href;
