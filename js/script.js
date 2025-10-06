bb5d730cf26c00c7587dbd4492101628d58dd5f8
v-eeru <133241025+v-eeru@users.noreply.github.com>
2025-10-06 21:36:57 +0530

    new new product

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-4YCPERINHU7C-2");
+  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-AABOV4O8K47Q-2");
 
 document.addEventListener("DOMContentLoaded", function () {
   const fullUrl = window.location.href;
